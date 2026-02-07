fa63ef64575c36005783eeea8e8abe0a074643cb
mosiur <mosiurzzaman99@gmail.com>
2026-02-07 18:51:06 +0600

    feat:ticket master booking service added

---
+++ ticket_master_booking_service/src/main/java/com/practice/ticket_master_booking_service/TicketMasterBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.practice.ticket_master_booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMasterBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketMasterBookingServiceApplication.class, args);
+	}
+
+}
---
+++ ticket_master_booking_service/src/test/java/com/practice/ticket_master_booking_service/TicketMasterBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.ticket_master_booking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMasterBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
