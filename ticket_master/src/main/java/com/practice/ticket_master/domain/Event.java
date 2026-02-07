5d2d65d2643098e16875ce9815d737a00ae9740f
mosiur <mosiurzzaman99@gmail.com>
2026-02-07 14:10:08 +0600

    feat:new domain classes added

---
+++ ticket_master/src/main/java/com/practice/ticket_master/domain/Event.java
@@ -0,0 +1,30 @@
+package com.practice.ticket_master.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "event")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "venue_id", nullable = false)
+    private Venue venue;
+
+    @ManyToOne
+    @JoinColumn(name = "performer_id", nullable = false)
+    private Performer performer;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+}
---
+++ ticket_master/src/main/java/com/practice/ticket_master/domain/Venue.java
@@ -0,0 +1,17 @@
+package com.practice.ticket_master.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "venue")
+public class Venue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String Location;
+}
